1d3a01bda370fe2f934aa06ace7fe8231ee75567
Koichiro Oishi <bs@v7.com>
2015-11-02 19:39:54 +0900

    add 1.3

--- Swift2GuideBook1_3.playground/Contents.swift
+++ Swift2GuideBook1_3.playground/Contents.swift
@@ -1,6 +1,7 @@
 //1-3-1
 
 import UIKit
+import XCPlayground
 
 // 1.1
 class SampleClass
@@ -19,3 +20,30 @@ sample = nil // deinitが呼ばれる
 for i in 0...10 {
   i * i
 }
+
+//1.3
+
+let circleRect = CGRectMake(0, 0, 200, 200)
+class CircleView : UIView
+{
+  override func drawRect(rect: CGRect)
+  {
+    let context:CGContextRef = UIGraphicsGetCurrentContext()!
+    
+    UIColor.whiteColor().setFill()
+    CGContextFillRect(context, rect)
+    
+    UIColor.blackColor().setFill()
+    CGContextFillEllipseInRect(context, rect)
+  }
+}
+
+let view = CircleView(frame: circleRect)
+
+// XCPShowView(_:view:) is deprecated. Set 'XCPlaygroundPage.liveView' instead.
+//XCPShowView("Live View", view: view)
+
+XCPlaygroundPage.currentPage.liveView = view
+
+
+
