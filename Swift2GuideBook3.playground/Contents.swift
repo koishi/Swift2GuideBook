b0ffd66810dbebc2cac7fea0b24cf3e1133fafe7
Koichiro Oishi <bs@v7.com>
2015-11-08 18:31:58 +0900

    add 317,319

--- Swift2GuideBook3.playground/Contents.swift
+++ Swift2GuideBook3.playground/Contents.swift
@@ -3,7 +3,7 @@ import UIKit
 class SampleClass
 {
   // 3-2-1 関数の型
-  class func func321()
+  class func func316()
   {
 
     // f1を変数test_f1に代入
@@ -24,6 +24,43 @@ class SampleClass
     test_f2(1000)
     
   }
+  
+  // クロージャによる変数キャプチャ例
+  class func func317()
+  {
+    var f1: Int -> Int
+    repeat {
+      
+      var captured = 10
+
+      f1 = {(f1: Int) -> Int in
+        return captured++ * f1
+      }
+    
+    } while false
+
+    f1(5)
+    f1(6)
+  }
+
+  class func func319()
+  {
+    let ff_int = {(i: Int, f:(Int -> Int)) -> Int in
+      return f(i)
+    }
+    
+    let sq_int = {(i: Int) -> Int in
+      return i * i
+    }
+
+    ff_int(5, sq_int)
+    
+  }
+  
 }
 
-SampleClass.func321()
+SampleClass.func316()
+
+SampleClass.func317()
+
+SampleClass.func319()
